48b0d016301e5b4a86a60648fa4dde0ef654434b
Abdessamad Antari <abantari@c3r9p7.1337.ma>
2026-01-23 12:08:04 +0100

    save

--- code_force/Bear_and_Big_Brother.cpp
+++ code_force/Bear_and_Big_Brother.cpp
@@ -1,3 +1,5 @@
+//https://codeforces.com/problemset/problem/791/A
+
 #include <iostream>
 #include <string>
 
--- code_force/Translation.cpp
+++ code_force/Translation.cpp
@@ -1,3 +1,4 @@
+//https://codeforces.com/problemset/problem/41/A
 #include <iostream>
 #include <string>
 #include <cstring>
